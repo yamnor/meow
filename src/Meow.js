4266b4eb6b2f6c5e41fa84fdd4b404d03762a6c1
yamnor <yamnor@gmail.com>
2024-09-30 18:40:29 +0900

    modify info message

--- src/Meow.js
+++ src/Meow.js
@@ -30,7 +30,7 @@ const Editor = ({ content, setContent }) => {
         defaultProtocol: 'https',
       }),
       Placeholder.configure({
-        placeholder: 'Write here...',
+        placeholder: 'Type or paste your text here...',
         emptyNodeClass: 'my-custom-is-empty-class',
       }),
     ],
@@ -72,8 +72,8 @@ const Meow = () => {
     setContent('')
   }
 
-  const openInfo = () => {
-    const content = "MQAgsgpg9g7ii8G4Lj2BQzigOoAsCGAXEASwGdxoYB+VAKmsllqNJwDsQIdiBPAWjyh4BXYhBD8oAGxAATCMUIBzFhGkgAZlABOIYrk2EWC9VoC2+PCrEQAHngB0IAJpRBIAMasxXAA6iDYrFEVQn5tVlU3CUI3AGsA0QADAGUACQBBACUAUQSQACNBPH42fhAFCGVNfFEcECiWGIcAFSwSOoM4tygWPBwDUggANwhNLjxWwxAuFwByYZAYfSKKgBp2Fhw8+qNp11LdHE0-AhgQrBcCI4kuAyNukFqRTWHNBwBJAm9NKEHCWWYdIQTN4JDUWKoRG5BEcFjguGIoDo9KJdtoLLY7KhnK4PGw3IFYg9Bn0JJtQexpCEtEitHgoXhSABtQJHAC6AAosEVvMQAFwAen5eEI3l63jsskG-OkUDcxH5wVC-K6Rx4XRYbggovlMQgXDyUEO0h4ulp9OIwC+KggagMKh4uv1hs0xtNmjphWIAEo1uMXAosCAWFACDgJFINJozEVbiAoKLCN1mDDiIJvN5aSpMWhQPQ4PBABA7azzCCLIAAioIcITAHwbgDDdmh0ciMGCcIgsYjeQhHVR5eHjUQwCB5B7pkCM2iV6sxWgcrl4HkC-l5CC9OwARyrsTs3hw5R97iO1V78MZAHEoFBVFlg4IAyAADIPgDCc+5fMFCkvslvAbswi9BxsEIclJy3GIZmIRgcHTb5q0DKA1CRQ5Y3VCwehAX5anqGJSE7Q5dVUAciFkWoVwmVRaDzWhfUCQ8QmiMMZEINQ1BGCpNSYAJ8BAKjm2oEBhDkB4QAwRwkjeMSzxAdlqGwHjsRAJIIFEN5SHkghFLPVdqC9CkqU0NYwwkWBY0jEATC0PwekEBjhjWI8JD4IEgkpYVDGzFJYCGEY1j4hgBNwPCoBMUQohMEJ8ETDt3BC3cewRXjqCnWJaEAFAIQhkKA5BYGYCFTdNaXUDg8GhYSol1IgzHKZjO1JLg1jMGJY0y2pLJTPAjxMeoVF9Gw8B4DQoREIjLwkbN0FE84LJwWQyFgKhkH8mBGDaMw5r7ccuBwExg00N8Fw-fltt2rQ7FCg9anxCAIuIAga0ARd3AFgCeBABpdnRfDcQgw0IAAvFq2Es0EoVJbR5BMQRSWFe4ol+SZ-AAKRgzxAB4NwB73ZRwBX3feJDdhAXB5mDYVNQhELVwmIxwgWFwJFUCrRFKI98QswczhATc5GhjtaP9LliCM8FYpMUKen5kA3hmOmfgZxFAkOdRvhMKYXAcRln26Xo3AIUKDsXQVQWWTQ8m7aQ7C6Y6dr2vTAEhyQB4P7WalbUF0K4zYAA-N2mlOQ2PfHAANXWjusM2Qot07NEAoA"
+  const setInfoText = () => {
+    const content = "MQAgsgpg9g7ii8G4Lj2BQzigOoAsCGAXEASwGdxoYB+VAKmsllqNJwDsQIdiBPAWjyh4BXYhBD8oAGxAATCMUIBzFhGkgAZlABOIYrk2EWC9VoC2+PCrEQAHngB0IAJpRBIAMasxXAA6iDYrFEVQn5tVlU3CUI3AGsA0QADAGUACQBBACUAUQSQACNBPH42fhAFCGVNfFEcECiWGIcAFSwSOoM4tygWPBwDUggANwhNLjxWwxAuFwByYZAYfSKKgBp2Fhw8+qNp11LdHE0-AhgQrBcCI4kuAyNukFqRTWHNBwBJAm9NKEHCWWYdIQTN4JDUWKoRG5BEcFjguGIoDo9KJdtoLLY7KhnK4PGw3IFYg9Bn0JJtQexpCEtEitHgoXhSABtQJHAC6AAosEVvMQAFwAen5eEI3l63jsskG-OkUDcxH5wVC-K6Rx4XRYbggovlMQgXDyUEO0h4ulp9OIwC+KggagMKh4uv1hs0xtNmjphWIAEo1uMXAosCAWFACDgJFINJozEVbiAoKLCN1mDDiIJvN5aSpMWhQPQ4PBABA7azzCCLIAAioIcITAHwbgDDdmh0ciMGCcIgsYjeQhHVR5eHjUQwCB5B7pkCM2iV6sxWgcrl4HkC-l5CC9OwARyrsTs3hw5R97iO1V78MZAHEoFBVFlg4IAyAADIPgDCc+5fMFCkvslvAbswi9BxsEIclJy3GIZmIRgcHTb5q0DKA1CRQ5Y3VCwehAX5anqGJSE7Q5dVUAciFkWoVwmVRaDzWhfUCQ8QmiMMZEINQ1BGCpNSYAJ8BAKjm2oEBhDkB4QAwRwkjeMSzxAdlqGwHjsRAJIIFEN5SHkghFLPVdqC9CkqU0NYwwkWBY0jEATC0PwekEBjhjWI8JD4IEgkpYVDGzFJYCGEY1j4hgBNwPCoBMUQohMEJ8ETDt3BC3cewRXjqCnWJaEAFAIQhkKA5BYGYCFTdNaXUDg8GhYSol1IgzHKZjO1JLg1jMGJY0y2pLJTPAjxMeoVF9Gw8B4DQoREIjLwkbN0FE85DxKyw8yoZB-JgFs2zcI8LBPccuBwExg00N8Fw-fltt2rQ7FCg9anxCAIuIAga0ARd3AFgCeBABpdnRfDcQgw0IAAvFq2Es0EoVJbR5BMQRSWFe4ol+SZ-AAKRgzxAB4NwB73ZRwBX3feJDdhAXB5lYeFNzkaGO1o-0uWINZqS6ExQp6aniogCMjlEUpGTQ6sCFCg7F0FUFlk0PJu2kOwumOna9r0wBIckAeD+420W1wQs0R7kZAANPmjuscWQsl07NEAoA"
     setContent(decodeContent(content))
   }
 
@@ -83,15 +83,9 @@ const Meow = () => {
         <Editor content={content} setContent={setContent} />
       </Box>
       <Box p={8} textAlign="center" position="sticky" bottom={20}>
-        <Button className="button" onClick={shareContent}>
-          <Link2 size={28} />
-        </Button>
-        <Button className="button" onClick={cleanContent}>
-          <FilePlus size={28} />
-        </Button>
-        <Button className="button" onClick={openInfo}>
-          <Info size={28} />
-        </Button>
+        <Button className="button" onClick={shareContent}><Link2 size={28} /></Button>
+        <Button className="button" onClick={cleanContent}><FilePlus size={28} /></Button>
+        <Button className="button" onClick={setInfoText}><Info size={28} /></Button>
       </Box>
     </main>
   )
